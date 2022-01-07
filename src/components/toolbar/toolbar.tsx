97655c9ba3beff7d5bcba9749c81093a45831964
Ștefan Țîncu <stefantnc11@gmail.com>
2022-01-07 23:19:07 +0200

    Configure toolbar for different views
    Each view has to display different actions on the toolbar based on its functionalities.
    
    Resolves: #6

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import Modal from "react-modal";
 
 type ToolbarProps = {
 	view: string;
@@ -7,6 +8,9 @@ type ToolbarProps = {
 
 const Toolbar: React.FC<ToolbarProps> = (props: ToolbarProps) => {
 	const history = useHistory();
+	const [showCategories, setShowCategories] = useState(false);
+	const [showRewards, setShowRewards] = useState(false);
+	const [showFilters, setShowFilters] = useState(false);
 
 	function displayActions(): JSX.Element {
 		switch (props.view) {
@@ -14,15 +18,24 @@ const Toolbar: React.FC<ToolbarProps> = (props: ToolbarProps) => {
 				return (
 					<div className="ToolbarView">
 						{/* Filter icon */}
-						<img className="ToolbarIcon" />
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => setShowFilters(true)}
+						/>
 						{/* Rewards icon */}
-						<img className="ToolbarIcon" />
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => setShowRewards(true)}
+						/>
 						{/* Categories icon */}
-						<img className="ToolbarIcon" />
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => setShowCategories(true)}
+						/>
 						{/* Calendar icon */}
 						<img
 							className="ToolbarIcon"
-							onClick={(): void => history.push("/settings")}
+							onClick={(): void => history.push("/calendar")}
 						/>
 						{/* Archive icon */}
 						<img
@@ -38,13 +51,67 @@ const Toolbar: React.FC<ToolbarProps> = (props: ToolbarProps) => {
 				);
 
 			case "archive":
-				return <div className="IconPlaceholder"></div>;
+				return (
+					<div className="ToolbarView">
+						{/* Calendar icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/calendar")}
+						/>
+						{/* Dashboard icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/dashboard")}
+						/>
+						{/* Settings icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/settings")}
+						/>
+					</div>
+				);
 
 			case "calendar":
-				return <div className="IconPlaceholder"></div>;
+				return (
+					<div className="ToolbarView">
+						{/* Archive icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/archive")}
+						/>
+						{/* Dashboard icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/dashboard")}
+						/>
+						{/* Settings icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/settings")}
+						/>
+					</div>
+				);
 
 			case "settings":
-				return <div className="IconPlaceholder"></div>;
+				return (
+					<div className="ToolbarView">
+						{/* Calendar icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/calendar")}
+						/>
+						{/* Dashboard icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/dashboard")}
+						/>
+						{/* Archive icon */}
+						<img
+							className="ToolbarIcon"
+							onClick={(): void => history.push("/archive")}
+						/>
+					</div>
+				);
 		}
 	}
 
